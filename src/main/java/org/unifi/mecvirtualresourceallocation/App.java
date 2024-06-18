8de66634e92146982eadc4adbea4f8d3d1177cb4
AlessioBugetti <alessiobugetti98@gmail.com>
2024-06-18 20:59:15 +0200

    Initialize Maven Project

---
+++ src/main/java/org/unifi/mecvirtualresourceallocation/App.java
@@ -0,0 +1,13 @@
+package org.unifi.mecvirtualresourceallocation;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
